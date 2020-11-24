9c2a207d7ff05d1860fd8ca63343af3e00905dac
Ady <ady@GMs-MacBook-Pro.local>
2020-11-24 05:08:04 -0800

    deploy

--- src/config.js
+++ src/config.js
@@ -1,11 +1,3 @@
-let apiPath
-
-if (process.env.NODE_ENV === 'production') {
-    apiPath = 'https://stark-sierra-08099.herokuapp.com'
-} else {
-    apiPath = 'http://localhost:8000'
-}
-
-export default {
-    API_ENDPOINT: apiPath,
-}
+module.exports = {
+    API_ENDPOINT: 'https://stark-sierra-08099.herokuapp.com',
+  };
